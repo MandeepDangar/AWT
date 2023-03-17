82754670f9fa2f98bd98483a2ef867918876e46c
Mandeep Dangar <mandeepdangar5138@gmail.com>
2023-03-17 22:17:42 +0530

    Connected with mongoDB Database

--- AWT/seoblog/backend/server.js
+++ AWT/seoblog/backend/server.js
@@ -1,30 +1,40 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// bring routes
+const blogRoutes = require('./routes/blog')
 
 // app
 
 const app = express();
 
+// db
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.DATABASE_CLOUD, { useNewUrlParser: true })
+  .then(() => console.log("DB connected"));
+
 // middlewares
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 // cors
-app.use(cors())
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
-// routes
-app.get('/api', (req, res) => {
-    res.json({time: new Date().toString()});
-});
+// routes middleware
+app.use('/api',blogRoutes);
 
 // port
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
